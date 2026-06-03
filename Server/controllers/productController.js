ffed85fe257aae7d36ef6a64c0538c1b2f660d97
hasnain-fts <hasnainnain7711@gmail.com>
2026-06-03 07:50:30 +0500

    Api for admin created

--- Server/controllers/productController.js
+++ Server/controllers/productController.js
@@ -10,10 +10,11 @@ const getProducts = async (req,res) => {
     }
 }
 
+
 const singleProduct = async  (req,res) => {
     try {
            const S_product = await Product.findById(req.params.id);
-           if (!S-product)
+           if (!S_product)
            {
                return res.status(404).json({message : 'Product not found'});
            }
@@ -22,4 +23,57 @@ const singleProduct = async  (req,res) => {
         res.status(500).json({message : 'Server Error'});
     }
 }
-module.exports = {getProducts,singleProduct};
+//POST APIS 
+
+const createProduct = async (req,res) => {
+    try {
+        const {name,price,category,stock,imageURL} = req.body;
+        const newProduct = new Product ({name,price,category,stock,imageURL});
+        const savedProduct = await newProduct.save();
+        res.status(201).json(savedProduct);
+    }catch(error){
+        res.status(400).json({ message : error.message})
+    }
+}
+
+//Put apis
+const updateProduct = async (req,res) => {
+    try{
+        const product = await Product.findById(req.params.id);
+        if(!product) {
+            res.status(404).json({message : "Product not found"})
+        }
+        product.name = req.body.name || product.name;
+        product.price = req.body.price || product.price;
+        product.category = req.body.category || product.category;
+        product.stock = req.body.stock || product.stock;
+        product.imageURL = req.body.imageURL || product.imageURL;
+        const savedProduct = await product.save();
+        res.status(200).json({message : savedProduct})
+    }catch(error) {
+        res.status(400).json({message: error.message })
+    }
+}
+
+//Deletion apis
+
+const deleted_product = async (req,res) => {
+    try{
+        const product = await Product.findById(req.params.id);
+        if(!product) {
+            res.status(404).json({message : "Product not found"})
+        }
+        const deleted_product = await product.deleteOne();
+        res.status(200).json ({message : deleted_product})
+
+    }catch(error){
+        res.status(400).json({message : error.message })
+    }
+}
+
+
+
+
+
+
+module.exports = {getProducts,singleProduct,createProduct,updateProduct,deleted_product};
--- Server/routes/productRoutes.js
+++ Server/routes/productRoutes.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const {getProducts,singleProduct} = require('../controllers/productController');
+const {getProducts,singleProduct,createProduct,updateProduct,deleted_product} = require('../controllers/productController');
 
 router.get('/',getProducts);
 router.get('/:id',singleProduct);
+//For admin level
+router.post('/admin/create',isAdmin,createProduct);
+router.put('/admin/update/:id',isAdmin,updateProduct);
+router.delete('/admin/delete/:id',isAdmin,deleted_product);
 
 module.exports = router;
